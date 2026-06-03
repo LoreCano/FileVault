9aba44e7de2a7564ebce3016c269a1e4c169089b
Lorenzo Canocchi <lorenzocanocchi9@gmail.com>
2026-06-03 14:50:43 +0200

    Commit finale e sistemazione grafica

--- File Server/server.js
+++ File Server/server.js
@@ -18,7 +18,8 @@ const frontendPath = path.join(__dirname, '../FrontEnd');
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve i file statici (HTML, CSS, JS)
 app.use(express.static(frontendPath));
